eac793e41396652ab8c003ea8b5baa3f64ecfdb8
saidhuljohny2 <saidhuljohny2>
2024-10-16 05:11:24 +0000

    updated

---
+++ session-wise/1.4 - Delta Tables.sql
@@ -0,0 +1,71 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC
+-- MAGIC ## Delta Lake
+-- MAGIC
+-- MAGIC - Delta Lake is an open-source storage layer developed by Databricks that brings ACID transactions (Atomicity, Consistency, Isolation, Durability) to Apache Spark and big data workloads. It enhances data lakes by providing reliability, scalability, and performance for batch and streaming data.
+-- MAGIC 	
+-- MAGIC ### Key Features of Delta Lake:
+-- MAGIC - ACID Transactions: Delta Lake allows concurrent reads and writes by different users while ensuring data consistency.
+-- MAGIC - Versioning: Delta Lake stores previous versions of the data, enabling time travel, which allows you to query older versions of your data or restore previous states.
+-- MAGIC - Schema Enforcement: It prevents bad data from entering your data lakes by enforcing schemas on write.
+-- MAGIC - Data Quality: Delta Lake ensures that only complete and accurate data is written, Builds upon standard data formats: Parquet + Json
+-- MAGIC - Streaming and Batch Processing: Supports both streaming and batch data processing seamlessly.
+-- MAGIC - Scalability: Designed to handle petabyte-scale datasets.
+-- MAGIC
+
+-- COMMAND ----------
+
+
+CREATE CATALOG IF NOT EXISTS dbx_catalog
+MANAGED LOCATION 'abfss://unity-catalog-storage@dbstorageyf6y4b6akjy6m.dfs.core.windows.net/170383088377031/dbx_catalog';
+
+-- COMMAND ----------
+
+USE CATALOG dbx_catalog;
+CREATE SCHEMA IF NOT EXISTS dbx_catalog.dbx_schema;
+
+-- COMMAND ----------
+
+-- DBTITLE 1,CREATE
+-- delta tables
+
+CREATE TABLE dbx_catalog.dbx_schema.employees(
+    id INT,
+    name STRING,
+    salary FLOAT
+);
+
+-- COMMAND ----------
+
+-- DBTITLE 1,INSERT
+INSERT INTO dbx_catalog.dbx_schema.employees
+VALUES 
+  (1, "Adam", 3500.0),
+  (2, "Sarah", 4020.5);
+
+INSERT INTO dbx_catalog.dbx_schema.employees
+VALUES
+  (3, "John", 2999.3),
+  (4, "Thomas", 4000.3);
+
+INSERT INTO dbx_catalog.dbx_schema.employees
+VALUES
+  (5, "Anna", 2500.0);
+
+INSERT INTO dbx_catalog.dbx_schema.employees
+VALUES
+  (6, "Kim", 6200.3);
+
+-- COMMAND ----------
+
+SELECT * FROM dbx_catalog.dbx_schema.employees
+
+-- COMMAND ----------
+
+-- DBTITLE 1,describe
+DESCRIBE DETAIL dbx_catalog.dbx_schema.employees;
+
+-- COMMAND ----------
+
+DESCRIBE HISTORY dbx_catalog.dbx_schema.employees;
